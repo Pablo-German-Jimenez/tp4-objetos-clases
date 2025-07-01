42b6de5825a309d412fdd27e61edb8bcac3842b9
linkin <pablogermanjimenez7@gmail.com>
2025-06-30 23:02:22 -0300

    finish excersice three

--- js/excersiceThree.js
+++ js/excersiceThree.js
@@ -1,26 +1,25 @@
 /*3-Escribe una clase que permita crear distintos objetos “rectángulos”, con las propiedades de alto y ancho, mas los métodos necesarios para modificar y mostrar sus propiedades, calcular el perímetro y el área
-*/
-class Rectangles{
-    constructor(heigth,width){
-        this._heigth= heigth;
-        this._width = width; 
-    }
-    perimeter(){
-       const resultPerimeter = this._heigth*2+this._width*2;
-    console.log(`The rectangle has one side with ${this._heigth} and other side of ${this._width},and has a perimeter of ${resultPerimeter}`)
-       
-        
-    }
-    
-    area(){
-        const resultArea = this._heigth*this._width;
-        return `The area of the rectangle is the sum of they, and is equal of: ${resultArea}`;
-    }
+ */
+class Rectangles {
+  constructor(heigth, width) {
+    this._heigth = heigth;
+    this._width = width;
+  }
+  perimeter() {
+    const resultPerimeter = this._heigth * 2 + this._width * 2;
+    console.log(
+      `The rectangle has a height of: ${this._heigth} and width of: ${this._width},and has a perimeter of ${resultPerimeter}`
+    );
+  }
+
+  area() {
+    const resultArea = this._heigth * this._width;
+    return `The area of the rectangle is the multiply of they, and is equal of: ${resultArea}`;
+  }
 }
 
+const rectanglesConst = new Rectangles(6, 3);
 
-const rectanglesConst = new Rectangles("6","3")
 
-console.log(rectanglesConst)
-console.log(rectanglesConst.perimeter())
-console.log(rectanglesConst.area())
+console.log(rectanglesConst.area());
+console.log(rectanglesConst.perimeter());
